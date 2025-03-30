ac852bd4b68739f13ecd637223f133420d7e0a12
FloSmt <109459476+FloSmt@users.noreply.github.com>
2025-03-30 12:38:01 +0200

    add environment variables for dev and prod in the backend

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,37 +1,26 @@
 import { Module } from '@nestjs/common';
 import {UserModule} from "./modules/user/user.module";
 import {AuthModule} from "./auth/auth.module";
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {Match} from "./database/entities/match.entity";
-import {Matchday} from "./database/entities/matchday.entity";
-import {Tip} from "./database/entities/tip.entity";
-import {Tipgroup} from "./database/entities/tipgroup.entity";
-import {TipgroupUser} from "./database/entities/tipgroupUser.entity";
-import {TipSeason} from "./database/entities/tipSeason.entity";
-import {User} from "./database/entities/user.entity";
 import {ConfigModule} from "@nestjs/config";
+import dbConfig from "./database/config/db.config";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import dbConfigProduction from "./database/config/db.config.production";
+import * as process from "node:process";
+import * as path from "node:path";
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3307,
-      username: 'root',
-      password: 'root',
-      database: 'tippApp-DB',
-      entities: [
-        // __dirname + '/../**/*.entity{.ts,.js}',
-        Match,
-        Matchday,
-        Tip,
-        Tipgroup,
-        TipgroupUser,
-        TipSeason,
-        User
-      ],
-      synchronize: true,
+    ConfigModule.forRoot({
+      envFilePath: path.resolve(
+        process.cwd(),
+        'environments',
+        `.env.${process.env.NODE_ENV || 'development'}`
+      ),
+      isGlobal: true,
+      load: [dbConfig, dbConfigProduction]
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: process.env.NODE_ENV === "production"? dbConfigProduction : dbConfig,
     }),
     UserModule,
     AuthModule
