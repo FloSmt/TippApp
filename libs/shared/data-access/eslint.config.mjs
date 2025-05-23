45f92e1ace37faa4d51ddabb1795c197bf5a9f24
FloSmt <florian.schmidt.schkeuditz@gmail.com>
2025-05-23 11:25:13 +0200

    adjust eslint configs

--- libs/shared/data-access/eslint.config.mjs
+++ libs/shared/data-access/eslint.config.mjs
@@ -2,21 +2,4 @@ import baseConfig from '../../../eslint.config.mjs';
 
 export default [
   ...baseConfig,
-  {
-    files: ['**/*.json'],
-    rules: {
-      '@nx/dependency-checks': [
-        'error',
-        {
-          ignoredFiles: [
-            '{projectRoot}/eslint.config.{js,cjs,mjs}',
-            '{projectRoot}/esbuild.config.{js,ts,mjs,mts}',
-          ],
-        },
-      ],
-    },
-    languageOptions: {
-      parser: await import('jsonc-eslint-parser'),
-    },
-  },
 ];
--- libs/shared/data-access/src/dtos/user/user-response.dto.ts
+++ libs/shared/data-access/src/dtos/user/user-response.dto.ts
@@ -1,4 +1,1 @@
-import {OmitType} from "@nestjs/swagger";
-import {User} from "@tippapp/backend/database";
-
-export class UserResponseDto extends OmitType(User, ['id', 'email', 'username'] as const) {}
+export class UserResponseDto {}
