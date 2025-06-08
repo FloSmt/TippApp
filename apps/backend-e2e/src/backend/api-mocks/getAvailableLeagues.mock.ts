bedac7753b1a90abaa7bae10b1bae1dc838800f4
FloSmt <109459476+FloSmt@users.noreply.github.com>
2025-06-08 20:39:25 +0200

    add availableLeagues mock to mockserver

---
+++ apps/backend-e2e/src/backend/api-mocks/getAvailableLeagues.mock.ts
@@ -0,0 +1,213 @@
+export const AVAILABLE_LEAGUES_MOCK =
+  [
+    {
+      "leagueId": 3,
+      "leagueName": "1. Fußball-Bundesliga 2007/2008",
+      "leagueShortcut": "bl1",
+      "leagueSeason": "2007",
+      "sport": {
+        "sportId": 1,
+        "sportName": "Fußball"
+      }
+    },
+    {
+      "leagueId": 4,
+      "leagueName": "2. Fussball-Bundesliga 2007/2008",
+      "leagueShortcut": "bl2",
+      "leagueSeason": "2007",
+      "sport": {
+        "sportId": 1,
+        "sportName": "Fußball"
+      }
+    },
+    {
+      "leagueId": 5,
+      "leagueName": "Uefa EM 2008 ",
+      "leagueShortcut": "fem08",
+      "leagueSeason": "2008",
+      "sport": {
+        "sportId": 1,
+        "sportName": "Fußball"
+      }
+    },
+    {
+      "leagueId": 6,
+      "leagueName": "1. Fussball-Bundesliga 2006/2007",
+      "leagueShortcut": "bl1",
+      "leagueSeason": "2006",
+      "sport": {
+        "sportId": 1,
+        "sportName": "Fußball"
+      }
+    },
+    {
+      "leagueId": 7,
+      "leagueName": "2. Fußball-Bundesliga 2006/2007",
+      "leagueShortcut": "bl2",
+      "leagueSeason": "2006",
+      "sport": {
+        "sportId": 1,
+        "sportName": "Fußball"
+      }
+    },
+    {
+      "leagueId": 8,
+      "leagueName": "DFB-Pokal 2007/2008",
+      "leagueShortcut": "dfb",
+      "leagueSeason": "2007",
+      "sport": {
+        "sportId": 1,
+        "sportName": "Fußball"
+      }
+    },
+    {
+      "leagueId": 11,
+      "leagueName": "1. Fußball-Bundesliga 2004/2005",
+      "leagueShortcut": "bl1",
+      "leagueSeason": "2004",
+      "sport": {
+        "sportId": 1,
+        "sportName": "Fußball"
+      }
+    },
+    {
+      "leagueId": 12,
+      "leagueName": "1. Fussball-Bundesliga 2005/2006",
+      "leagueShortcut": "bl1",
+      "leagueSeason": "2005",
+      "sport": {
+        "sportId": 1,
+        "sportName": "Fußball"
+      }
+    },
+    {
+      "leagueId": 16,
+      "leagueName": "Uefa EM 2008 - Brinkwerth",
+      "leagueShortcut": "em_bri",
+      "leagueSeason": "2008",
+      "sport": {
+        "sportId": 1,
+        "sportName": "Fußball"
+      }
+    },
+    {
+      "leagueId": 18,
+      "leagueName": "1. Fussball-Bundesliga 2008/2009",
+      "leagueShortcut": "bl1",
+      "leagueSeason": "2008",
+      "sport": {
+        "sportId": 1,
+        "sportName": "Fußball"
+      }
+    },
+    {
+      "leagueId": 19,
+      "leagueName": "2. Fußball-Bundesliga 2008/2009",
+      "leagueShortcut": "bl2",
+      "leagueSeason": "2008",
+      "sport": {
+        "sportId": 1,
+        "sportName": "Fußball"
+      }
+    },
+    {
+      "leagueId": 25,
+      "leagueName": "3. Liga 2008/2009",
+      "leagueShortcut": "bl3",
+      "leagueSeason": "2008",
+      "sport": {
+        "sportId": 1,
+        "sportName": "Fußball"
+      }
+    },
+    {
+      "leagueId": 26,
+      "leagueName": "Regionalliga West 2008/2009",
+      "leagueShortcut": "rlw",
+      "leagueSeason": "2008",
+      "sport": {
+        "sportId": 1,
+        "sportName": "Fußball"
+      }
+    },
+    {
+      "leagueId": 27,
+      "leagueName": "DFB-Pokal 2008/2009",
+      "leagueShortcut": "dfb",
+      "leagueSeason": "2008",
+      "sport": {
+        "sportId": 1,
+        "sportName": "Fußball"
+      }
+    },
+    {
+      "leagueId": 31,
+      "leagueName": "Axpo Super League 2008/2009",
+      "leagueShortcut": "asl",
+      "leagueSeason": "2008",
+      "sport": {
+        "sportId": 1,
+        "sportName": "Fußball"
+      }
+    },
+    {
+      "leagueId": 32,
+      "leagueName": "Öst. Bundesliga 2008/2009",
+      "leagueShortcut": "ÖFB",
+      "leagueSeason": "2008",
+      "sport": {
+        "sportId": 1,
+        "sportName": "Fußball"
+      }
+    },
+    {
+      "leagueId": 43,
+      "leagueName": "Champions League",
+      "leagueShortcut": "cl",
+      "leagueSeason": "2008",
+      "sport": {
+        "sportId": 1,
+        "sportName": "Fußball"
+      }
+    },
+    {
+      "leagueId": 44,
+      "leagueName": "Bayernliga",
+      "leagueShortcut": "BAY-L",
+      "leagueSeason": "2008",
+      "sport": {
+        "sportId": 1,
+        "sportName": "Fußball"
+      }
+    },
+    {
+      "leagueId": 47,
+      "leagueName": "DFB Pokal Endrunde 2008/2009",
+      "leagueShortcut": "dfb_end",
+      "leagueSeason": "2008",
+      "sport": {
+        "sportId": 1,
+        "sportName": "Fußball"
+      }
+    },
+    {
+      "leagueId": 49,
+      "leagueName": "1. Fußball-Bundesliga 2003/2004",
+      "leagueShortcut": "bl1",
+      "leagueSeason": "2003",
+      "sport": {
+        "sportId": 1,
+        "sportName": "Fußball"
+      }
+    },
+    {
+      "leagueId": 50,
+      "leagueName": "1. Fußball-Bundesliga 2002/2003",
+      "leagueShortcut": "bl1",
+      "leagueSeason": "2002",
+      "sport": {
+        "sportId": 1,
+        "sportName": "Fußball"
+      }
+    }
+  ]
--- apps/backend-e2e/src/backend/helper/mockserver.helper.ts
+++ apps/backend-e2e/src/backend/helper/mockserver.helper.ts
@@ -1,16 +1,19 @@
 import {mockServerClient} from 'mockserver-client';
 import {MATCHDATA_MOCK} from "../api-mocks/getMatchData.mock";
 import {AVAILABLE_GROUPS_MOCK} from "../api-mocks/getAvailableGroups.mock";
+import {AVAILABLE_LEAGUES_MOCK} from "../api-mocks/getAvailableLeagues.mock";
 
 export interface MockApiOptions {
   matchDataResponse?: any,
   availableGroupsResponse?: any,
+  availableLeaguesResponse?: any,
   errorCode?: number | undefined,
 }
 
 export const setupMockApi = async (options: MockApiOptions = {
   matchDataResponse: MATCHDATA_MOCK,
   availableGroupsResponse: AVAILABLE_GROUPS_MOCK,
+  availableLeaguesResponse: AVAILABLE_LEAGUES_MOCK,
   errorCode: undefined
 }) => {
   const client = mockServerClient('localhost', 1080);
@@ -25,6 +28,20 @@ export const setupMockApi = async (options: MockApiOptions = {
       body: JSON.stringify(options.matchDataResponse),
       headers: [
         { name: 'Content-Type', values: ['application/json'] }
+      ]
+    },
+  });
+
+  await client.mockAnyResponse({
+    httpRequest: {
+      method: 'GET',
+      path: '/getavailableleagues/',
+    },
+    httpResponse: {
+      statusCode: options.errorCode ?? 200,
+      body: JSON.stringify(options.availableLeaguesResponse),
+      headers: [
+        {name: 'Content-Type', values: ['application/json']}
       ]
     },
   });
