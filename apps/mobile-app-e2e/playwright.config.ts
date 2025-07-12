4beafb53009429c98500880f231ffb2f350d03c1
FloSmt <109459476+FloSmt@users.noreply.github.com>
2025-07-12 19:36:39 +0200

    WIP fix unit-tests

--- apps/mobile-app-e2e/playwright.config.ts
+++ apps/mobile-app-e2e/playwright.config.ts
@@ -1,7 +1,5 @@
 // libs/frontend/frontend/ui/shared-components/playwright.config.ts
-import {defineConfig, devices} from '@playwright/test';
-import {nxE2EPreset} from '@nx/playwright/preset';
-import {workspaceRoot} from "nx/src/utils/workspace-root";
+import {defineConfig} from '@playwright/test';
 
 // For CI, you may want to set BASE_URL to the deployed application.
 const baseURL = process.env['BASE_URL'] || 'http://localhost:4200';
@@ -16,56 +14,4 @@ const baseURL = process.env['BASE_URL'] || 'http://localhost:4200';
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
-export default defineConfig({
-  ...nxE2EPreset(__filename, { testDir: './src' }),
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    baseURL,
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-  },
-  /* Run your local dev server before starting the tests */
-  // Temporarily commenting out webServer to diagnose "invalid type: unit value" error
-  webServer: {
-    command: 'npx nx run mobile-app:serve',
-    url: 'http://localhost:4200',
-    reuseExistingServer: !process.env['CI'],
-    cwd: workspaceRoot,
-  },
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-
-    // Uncomment for mobile browsers support
-    {
-      name: 'Mobile Chrome',
-      use: { ...devices['Pixel 5'] },
-    },
-    /*{
-      name: 'Mobile Safari',
-      use: { ...devices['iPhone 12'] },
-    },
-
-    // Uncomment for branded browsers
-    /* {
-      name: 'Microsoft Edge',
-      use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    },
-    {
-      name: 'Google Chrome',
-      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    } */
-  ],
-});
+export default defineConfig({});
