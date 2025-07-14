5da2f555c138cafe086b5271542ba1c8db25c537
FloSmt <florian.schmidt.schkeuditz@gmail.com>
2025-07-14 17:12:57 +0200

    remove ios ui-tests

--- apps/mobile-app-e2e/playwright.config.ts
+++ apps/mobile-app-e2e/playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npx nx serve mobile-app',
+    command: 'npx nx serve mobile-app:production',
     url: 'http://localhost:4200',
     reuseExistingServer: !!process.env.CI,
     cwd: workspaceRoot,
