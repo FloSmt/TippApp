6d19ae3182c04434b396333f65065c5206f18e35
FloSmt <florian.schmidt.schkeuditz@gmail.com>
2025-07-21 18:06:12 +0200

    WIP: start FormControl of Register page

--- libs/frontend/auth/src/pages/register-page/register-page.component.ts
+++ libs/frontend/auth/src/pages/register-page/register-page.component.ts
@@ -1,12 +1,74 @@
-import {Component} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {IonicModule} from '@ionic/angular';
-import {ReactiveFormsModule} from '@angular/forms';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'lib-register-page',
-  imports: [CommonModule, IonicModule, ReactiveFormsModule],
+  imports: [CommonModule, IonicModule, ReactiveFormsModule, FormsModule],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.scss',
 })
-export class RegisterPageComponent {}
+export class RegisterPageComponent {
+  registerForm = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+    confirmPassword: new FormControl('', [
+      Validators.required,
+      this.confirmPasswordValidator(),
+    ]),
+  });
+
+  onRegister() {
+    // Handle registration logic here
+    console.log('Registration form submitted');
+  }
+
+  disableRegistration(): boolean {
+    return this.registerForm.invalid;
+  }
+
+  getErrorMessage(controlName: string): string {
+    const control = this.registerForm.get(controlName);
+    if (control?.hasError('required')) {
+      return `Dieses Feld ist erforderlich.`;
+    }
+    if (control?.hasError('minlength')) {
+      return `Nutzername muss mindestens ${control.errors?.['minlength'].requiredLength} Zeichen lang sein.`;
+    }
+    if (control?.hasError('email')) {
+      return 'Bitte geben Sie eine gültige E-Mail-Adresse ein.';
+    }
+
+    if (control?.hasError('passwordMismatch')) {
+      return 'Die Passwörter stimmen nicht überein.';
+    }
+    return '';
+  }
+
+  confirmPasswordValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const formGroup = control.parent as FormGroup;
+      if (!formGroup) return null;
+      const password = formGroup.get('password')?.value;
+      const confirmPassword = control.value;
+      return password !== confirmPassword ? { passwordMismatch: true } : null;
+    };
+  }
+}
