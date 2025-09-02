1f31a905f6e596d85a65e6263b9bee92ccb8d887
FloSmt <florian.schmidt.schkeuditz@gmail.com>
2025-09-02 19:51:53 +0200

    add environment configurations for angular frontend

--- libs/frontend/shared/utils/src/auth/auth.service.ts
+++ libs/frontend/shared/utils/src/auth/auth.service.ts
@@ -1,29 +1,46 @@
-import {Injectable} from '@angular/core';
-import {AuthResponseDto, LoginDto, RegisterDto} from '@tippapp/shared/data-access';
-import {Observable} from 'rxjs';
-import {HttpClient} from "@angular/common/http";
-import {Router} from "@angular/router";
+import { inject, Injectable } from '@angular/core';
+import {
+  AuthResponseDto,
+  LoginDto,
+  RegisterDto,
+} from '@tippapp/shared/data-access';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { ENVIRONMENT } from '../environments/environment.token';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  private readonly env = inject(ENVIRONMENT);
 
-  readonly BACKEND_URL = 'http://localhost:3000/api/'; //TODO: add to environment-variables
+  readonly BACKEND_URL = this.env.apiUrl;
 
-  constructor(public httpClient: HttpClient, public router: Router) {
-  }
+  constructor(public httpClient: HttpClient, public router: Router) {}
 
   registerNewUser(registerDto: RegisterDto): Observable<AuthResponseDto> {
-    return this.httpClient.post<AuthResponseDto>(this.BACKEND_URL + 'auth/register', registerDto, {withCredentials: true});
+    return this.httpClient.post<AuthResponseDto>(
+      this.BACKEND_URL + 'auth/register',
+      registerDto,
+      { withCredentials: true }
+    );
   }
 
   loginUser(loginDto: LoginDto): Observable<AuthResponseDto> {
-    return this.httpClient.post<AuthResponseDto>(this.BACKEND_URL + 'auth/login', loginDto, {withCredentials: true});
+    return this.httpClient.post<AuthResponseDto>(
+      this.env.apiUrl + 'auth/login',
+      loginDto,
+      { withCredentials: true }
+    );
   }
 
   refreshAccessToken(): Observable<AuthResponseDto> {
-    return this.httpClient.post<AuthResponseDto>(this.BACKEND_URL + 'auth/refresh', {}, {withCredentials: true})
+    return this.httpClient.post<AuthResponseDto>(
+      this.env.apiUrl + 'auth/refresh',
+      {},
+      { withCredentials: true }
+    );
   }
 
   logoutAndRedirect() {
--- libs/frontend/shared/utils/src/tipgroup/tipgroup.service.ts
+++ libs/frontend/shared/utils/src/tipgroup/tipgroup.service.ts
@@ -1,18 +1,22 @@
-import {inject, Injectable} from '@angular/core';
-import {Observable} from "rxjs";
-import {TipgroupEntryResponseDto} from "@tippapp/shared/data-access";
-import {HttpClient} from "@angular/common/http";
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TipgroupEntryResponseDto } from '@tippapp/shared/data-access';
+import { HttpClient } from '@angular/common/http';
+import { ENVIRONMENT } from '../environments/environment.token';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TipgroupService {
-  readonly BACKEND_URL = 'http://localhost:3000/api/'; //TODO: add to environment-variables
-
   readonly httpClient = inject(HttpClient);
+  private readonly env = inject(ENVIRONMENT);
+
+  readonly BACKEND_URL = this.env.apiUrl;
 
   getAvailableTipgroups(): Observable<TipgroupEntryResponseDto[]> {
     console.log('TipgroupService: getAvailableTipgroups');
-    return this.httpClient.get<TipgroupEntryResponseDto[]>(this.BACKEND_URL + 'user/tipgroups');
+    return this.httpClient.get<TipgroupEntryResponseDto[]>(
+      this.BACKEND_URL + 'user/tipgroups'
+    );
   }
 }
