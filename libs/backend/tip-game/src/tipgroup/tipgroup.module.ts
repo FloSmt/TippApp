367c25e4ba939cec8ec7a1078fd699801d5bec2b
FloSmt <florian.schmidt.schkeuditz@gmail.com>
2025-11-21 20:36:25 +0100

    fix lint errors

--- libs/backend/tip-game/src/tipgroup/tipgroup.module.ts
+++ libs/backend/tip-game/src/tipgroup/tipgroup.module.ts
@@ -4,6 +4,7 @@ import { Tipgroup } from '@tippapp/shared/data-access';
 import { ApiModule } from '@tippapp/backend/api';
 import { UserModule } from '@tippapp/backend/user';
 import { ErrorManagerService } from '@tippapp/backend/error-handling';
+import { HashService } from '@tippapp/backend/shared';
 import { TipSeasonModule } from '../tipseason';
 import { TipgroupsController } from './tipgroups.controller';
 import { TipgroupService } from './tipgroup.service';
@@ -17,6 +18,6 @@ import { TipgroupService } from './tipgroup.service';
   ],
   controllers: [TipgroupsController],
   exports: [TipgroupService],
-  providers: [TipgroupService, ErrorManagerService],
+  providers: [TipgroupService, ErrorManagerService, HashService],
 })
 export class TipgroupModule {}
