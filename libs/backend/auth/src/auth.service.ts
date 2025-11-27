cadb6da3ba45d91dc65c4794826c88225e0766ad
FloSmt <florian.schmidt.schkeuditz@gmail.com>
2025-11-27 22:46:58 +0100

    set @nestjs/jwt

--- libs/backend/auth/src/auth.service.ts
+++ libs/backend/auth/src/auth.service.ts
@@ -10,6 +10,7 @@ import { HashService } from '@tippapp/backend/shared';
 export interface JwtPayload {
   email: string;
   id: number;
+  refreshId?: string;
 }
 
 @Injectable()
@@ -22,27 +23,16 @@ export class AuthService {
     private readonly errorManager: ErrorManagerService
   ) {}
 
-  async login(
-    loginDto: LoginDto
-  ): Promise<{ accessToken: string; refreshToken: string }> {
+  async login(loginDto: LoginDto): Promise<{ accessToken: string; refreshToken: string }> {
     const user = await this.userService.findByEmail(loginDto.email);
 
     if (!user) {
-      throw this.errorManager.createError(
-        ErrorCodes.Auth.USER_NOT_FOUND,
-        HttpStatus.NOT_FOUND
-      );
+      throw this.errorManager.createError(ErrorCodes.Auth.USER_NOT_FOUND, HttpStatus.NOT_FOUND);
     }
 
-    const isPasswordMatch = await this.hashService.comparePasswords(
-      loginDto.password,
-      user.password
-    );
+    const isPasswordMatch = await this.hashService.comparePasswords(loginDto.password, user.password);
     if (!isPasswordMatch) {
-      throw this.errorManager.createError(
-        ErrorCodes.Auth.INVALID_CREDENTIALS,
-        HttpStatus.UNAUTHORIZED
-      );
+      throw this.errorManager.createError(ErrorCodes.Auth.INVALID_CREDENTIALS, HttpStatus.UNAUTHORIZED);
     }
 
     const newTokens = this.generateTokens({ id: user.id, email: user.email });
@@ -54,21 +44,13 @@ export class AuthService {
     };
   }
 
-  async register(
-    registerDto: RegisterDto
-  ): Promise<{ refreshToken: string; accessToken: string }> {
-    const userAlreadyExists =
-      (await this.userService.findByEmail(registerDto.email)) != null;
+  async register(registerDto: RegisterDto): Promise<{ refreshToken: string; accessToken: string }> {
+    const userAlreadyExists = (await this.userService.findByEmail(registerDto.email)) != null;
 
     if (userAlreadyExists) {
-      throw this.errorManager.createError(
-        ErrorCodes.Auth.EMAIL_ALREADY_EXISTS,
-        HttpStatus.CONFLICT
-      );
+      throw this.errorManager.createError(ErrorCodes.Auth.EMAIL_ALREADY_EXISTS, HttpStatus.CONFLICT);
     }
-    const passwordHash = await this.hashService.hashPassword(
-      registerDto.password
-    );
+    const passwordHash = await this.hashService.hashPassword(registerDto.password);
     const user = await this.userService.create({
       ...registerDto,
       password: passwordHash,
@@ -82,30 +64,17 @@ export class AuthService {
     };
   }
 
-  async refreshTokens(
-    refreshToken: string
-  ): Promise<{ accessToken: string; refreshToken: string }> {
-    const decodedToken: { email: string; id: number } | null =
-      this.verifyToken(refreshToken);
+  async refreshTokens(refreshToken: string): Promise<{ accessToken: string; refreshToken: string }> {
+    const decodedToken: { email: string; id: number } | null = this.verifyToken(refreshToken);
 
-    if (
-      !decodedToken ||
-      typeof decodedToken === 'string' ||
-      !('id' in decodedToken)
-    ) {
-      throw this.errorManager.createError(
-        ErrorCodes.Auth.INVALID_REFRESH_TOKEN,
-        HttpStatus.UNAUTHORIZED
-      );
+    if (!decodedToken || typeof decodedToken === 'string' || !('id' in decodedToken)) {
+      throw this.errorManager.createError(ErrorCodes.Auth.INVALID_REFRESH_TOKEN, HttpStatus.UNAUTHORIZED);
     }
 
     const user = await this.userService.findById(decodedToken.id);
 
     if (!user || !refreshToken || user.refreshToken !== refreshToken) {
-      throw this.errorManager.createError(
-        ErrorCodes.Auth.INVALID_REFRESH_TOKEN,
-        HttpStatus.UNAUTHORIZED
-      );
+      throw this.errorManager.createError(ErrorCodes.Auth.INVALID_REFRESH_TOKEN, HttpStatus.UNAUTHORIZED);
     }
 
     const newTokens = this.generateTokens({
