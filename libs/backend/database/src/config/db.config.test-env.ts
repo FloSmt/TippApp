7323e860c51a3bfe57c1b2303884a209c8b53ce7
FloSmt <florian.schmidt.schkeuditz@gmail.com>
2025-12-24 13:14:09 +0100

    add Docker images for backend and run them in github actions

--- libs/backend/database/src/config/db.config.test-env.ts
+++ libs/backend/database/src/config/db.config.test-env.ts
@@ -1,22 +1,19 @@
+import * as process from 'node:process';
 import { registerAs } from '@nestjs/config';
-import { SqliteConnectionOptions } from 'typeorm/driver/sqlite/SqliteConnectionOptions';
-import {
-  Match,
-  Matchday,
-  Tip,
-  Tipgroup,
-  TipgroupUser,
-  TipSeason,
-  User,
-} from '@tippapp/shared/data-access';
+import { Match, Matchday, Tip, Tipgroup, TipgroupUser, TipSeason, User } from '@tippapp/shared/data-access';
+import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
 
 export default registerAs(
   'dbconfig.test',
-  (): SqliteConnectionOptions => ({
-    type: 'sqlite',
-    database: ':memory:',
+  (): MysqlConnectionOptions => ({
+    type: 'mysql',
+    host: process.env['DB_HOST'] ?? 'localhost',
+    port: +(process.env['DB_PORT'] ?? 3306),
+    username: process.env['DB_USER'],
+    password: process.env['DB_PASS'],
+    database: process.env['DB_NAME'],
     entities: [User, Tipgroup, TipgroupUser, Match, Matchday, TipSeason, Tip],
     synchronize: true,
-    logging: false,
+    dropSchema: false,
   })
 );
