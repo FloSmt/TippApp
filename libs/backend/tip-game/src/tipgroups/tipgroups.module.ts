786af46b17d923d9e8f2c7e570dc649906c7a14d
FloSmt <florian.schmidt.schkeuditz@gmail.com>
2025-12-25 17:14:43 +0100

    fix backend providers

--- libs/backend/tip-game/src/tipgroups/tipgroups.module.ts
+++ libs/backend/tip-game/src/tipgroups/tipgroups.module.ts
@@ -4,13 +4,20 @@ import { Match, Matchday, Tipgroup, TipgroupUser, TipSeason } from '@tippapp/sha
 import { ApiModule } from '@tippapp/backend/api';
 import { UserModule } from '@tippapp/backend/user';
 import { ErrorManagerService } from '@tippapp/backend/error-handling';
-import { HashService } from '@tippapp/backend/shared';
+import {
+  HashService,
+  MatchdayRepository,
+  SeasonRepository,
+  TipgroupRepository,
+  TipgroupUserRepository,
+} from '@tippapp/backend/shared';
 import { SeasonController, SeasonService } from './season';
 import { TipgroupsController } from './tipgroups.controller';
 import { TipgroupsService } from './tipgroups.service';
 import { MatchdayController } from './matchday/matchday.controller';
 import { MatchdayService } from './matchday/matchday.service';
 import { IsTipgroupMemberGuard } from '../guards/is-tipgroup-member.guard.service';
+import { TipgroupMembersService } from '../tipgroup-members/tipgroup-members.service';
 
 @Module({
   imports: [
@@ -22,10 +29,15 @@ import { IsTipgroupMemberGuard } from '../guards/is-tipgroup-member.guard.servic
   exports: [TipgroupsService, MatchdayService, SeasonService],
   providers: [
     TipgroupsService,
+    TipgroupMembersService,
     ErrorManagerService,
     HashService,
     MatchdayService,
     SeasonService,
+    TipgroupRepository,
+    TipgroupUserRepository,
+    MatchdayRepository,
+    SeasonRepository,
     IsTipgroupMemberGuard,
   ],
 })
--- libs/backend/user/src/user.module.ts
+++ libs/backend/user/src/user.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TipgroupUser, User } from '@tippapp/shared/data-access';
+import { UserRepository } from '@tippapp/backend/shared';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, TipgroupUser])],
-  providers: [UserService],
+  providers: [UserService, UserRepository],
   exports: [UserService],
   controllers: [UserController],
 })
